c461f582e43f457effc30fe18fbf913542540f2e
Leo <leonardo.alvirubro@hotmail.com>
2025-05-07 18:06:58 -0300

    feat: Now working properly, but still need to change from PDF to a CBZ/ZIP file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -21,78 +22,8 @@ var (
 )
 
 func main() {
+	// Chama a funcao que vai fazer o Scrape
 	scrape()
-	var chapterURL string
-	c := colly.NewCollector()
-
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		//fmt.Println("Link encontrado:", e.Text, "→", e.Attr("href"))
-
-		if strings.Contains(e.Text, targetChapter) {
-			chapterURL = baseURL + e.Attr("href")
-			fmt.Println("Encontrado:", chapterURL)
-			//goToChapter(chapterURL)
-		}
-	})
-
-	// Visita a página principal do mangá
-	// err := c.Visit(mainURL)
-	// if err != nil {
-	// 	log.Fatal("Erro ao visitar página principal:", err)
-	// }
-	// if chapterURL == "" {
-	// 	log.Fatal("Capítulo não encontrado!")
-	// }
-
-	// // Agora visita o link do capítulo e coleta as imagens
-	// imageLinks := []string{}
-	// chapterCollector := colly.NewCollector()
-
-	// chapterCollector.OnHTML("img[src]", func(e *colly.HTMLElement) {
-	// 	src := e.Attr("src")
-	// 	if strings.HasPrefix(src, "https://") && (strings.HasSuffix(src, ".jpg") || strings.HasSuffix(src, ".png")) {
-	// 		imageLinks = append(imageLinks, src)
-	// 	}
-	// })
-
-	// err = chapterCollector.Visit(chapterURL)
-	// if err != nil {
-	// 	log.Fatal("Erro ao visitar o capítulo:", err)
-	// }
-
-	// if len(imageLinks) == 0 {
-	// 	log.Fatal("Nenhuma imagem encontrada.")
-	// }
-
-	// fmt.Printf("Total de imagens: %d\n", len(imageLinks))
-
-	// // Baixa imagens e cria o PDF
-	// pdf := gopdf.GoPdf{}
-	// pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
-
-	// for i, imgURL := range imageLinks {
-	// 	filename := fmt.Sprintf("page_%d.jpg", i)
-	// 	err := downloadImage(imgURL, filename)
-	// 	if err != nil {
-	// 		log.Println("Erro ao baixar imagem:", imgURL, err)
-	// 		continue
-	// 	}
-
-	// 	pdf.AddPage()
-	// 	//err = pdf.Image(filename, 0, 0, 595.28, 841.89) // A4
-	// 	err = pdf.Image(filename, 0, 0, &gopdf.Rect{W: 595.28, H: 841.89})
-	// 	if err != nil {
-	// 		log.Println("Erro ao adicionar imagem ao PDF:", err)
-	// 	}
-	// 	os.Remove(filename)
-	// }
-
-	// err = pdf.WritePdf("one_piece_ch_1146.pdf")
-	// if err != nil {
-	// 	log.Fatal("Erro ao salvar PDF:", err)
-	// }
-
-	// fmt.Println("PDF gerado com sucesso!")
 }
 
 func downloadImage(url, filepath string) error {
@@ -113,8 +44,24 @@ func downloadImage(url, filepath string) error {
 }
 
 func scrape() {
-	url := "https://mangapark.net/title/10953-en-one-piece/9625399-vol-tbe-ch-1146"
+	var chapterURL string
 
+	c := colly.NewCollector()
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		if strings.Contains(e.Text, targetChapter) {
+			chapterURL = baseURL + e.Attr("href")
+			fmt.Println("Capítulo encontrado:", chapterURL)
+		}
+	})
+	err := c.Visit(mainURL)
+	if err != nil {
+		log.Fatal("Erro ao visitar página principal:", err)
+	}
+	if chapterURL == "" {
+		log.Fatal("Capítulo não encontrado!")
+	}
+
+	// Acessando a pagina do cap pra pegar as imgs, ja que antes nao taava funcionando com 100% colly (talvez pq as imgs carregavam no JS ou algo do tipo, mas com o chromeDP funcionou)
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
@@ -123,13 +70,13 @@ func scrape() {
 
 	var imageLinks []string
 	// Executa o Chrome
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(url),
+	err = chromedp.Run(ctx,
+		chromedp.Navigate(chapterURL),
 		chromedp.WaitVisible(`img`, chromedp.ByQuery),
 		chromedp.Evaluate(`Array.from(document.querySelectorAll('img')).map(img => img.src)`, &imageLinks),
 	)
 	if err != nil {
-		panic(err)
+		log.Fatal("Erro ao extrair imagens com chromedp:", err)
 	}
 
 	if len(imageLinks) == 0 {
@@ -148,13 +95,15 @@ func scrape() {
 		err := downloadImage(link, filename)
 		if err == nil {
 			filenames = append(filenames, filename)
+		} else {
+			log.Println("Erro ao baixar imagem:", link, err)
 		}
 	}
 
 	// Gera o PDF
-	err = imagesToPDF(filenames, "capitulo_1146.pdf")
+	err = imagesToPDF(filenames, "one_piece_chapter_1146.pdf")
 	if err != nil {
-		panic(err)
+		log.Fatal("Erro ao gerar PDF:", err)
 	}
 
 	fmt.Println("PDF gerado com sucesso!")
@@ -177,8 +126,8 @@ func imagesToPDF(images []string, output string) error {
 X Entrar no site (ja especificado qual maanga)
 X Percorrer a lista toda de todos os capitulos
 X Listar todos os links (possiveis capitulos que posso pegar)
-Entrar nesses capitulos
-Baixar as imagens
-Salvar como PDF (ou talvez mando individualmente, ver qual é melhor)
+X Entrar nesses capitulos
+X Baixar as imagens
+X Salvar como PDF (ou talvez mando individualmente, ver qual é melhor)
 Tambem enviar os dados como releaseDate e o nome/title do capitulo, pra poder usar no meu site todas essas informacoes
 */
